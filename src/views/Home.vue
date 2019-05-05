50969bc339a39331a47db31add22d7437bba328e
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-05-05 12:06:03 -0300

    Adding new UI with searchbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/images/kamping-logo.png" />
+  <div class="home h-screen flex items-center">
+    <div class="w-full max-w-md mx-auto px-2">
+      <img
+        alt="Kamping logo"
+        class="w-64 mx-auto mb-6"
+        src="../assets/images/kamping-logo.png"
+      />
+      <p class="font-light text-grey-500 text-lg mb-2">
+        Find the best campinground in...
+      </p>
+      <div class="flex justify-between">
+        <input
+          type="search"
+          name="search"
+          id="search"
+          class="border border-grey-100  px-2 py-2 text-grey-500 text-lg font-light w-full"
+          placeholder="anywhere"
+        />
+        <button
+          class="bg-yellow-vivid-500 py-2 px-4 text-grey-900 font-semibold"
+        >
+          Find
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,6 +31,6 @@
 // @ is an alias to /src
 
 export default {
-  name: "home"
-};
+  name: 'home'
+}
 </script>
