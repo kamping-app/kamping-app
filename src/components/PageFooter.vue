8fd4ec93d93eb44127cd4f8b6900ace27dce6f86
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-05-16 00:14:00 -0300

    Create layout components

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class=" bg-yellow-vivid-500">
+    <wrapper>
+      <router-link to="/">
+        <img
+          alt="Kamping logo"
+          class="h-6"
+          src="../assets/images/kamping-logo.png"
+        />
+      </router-link>
+    </wrapper>
+  </footer>
+</template>
+
+<script>
+import Wrapper from '@/components/Wrapper'
+export default {
+  components: {
+    wrapper: Wrapper
+  }
+}
+</script>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <wrapper class="flex justify-between items-center">
+      <router-link to="/">
+        <img
+          alt="Kamping logo"
+          class="h-12"
+          src="../assets/images/kamping-logo.png"
+        />
+      </router-link>
+      <nav>
+        <router-link to="/">Principais cidades</router-link>
+        <router-link to="/">Blog</router-link>
+        <router-link to="/">Login</router-link>
+      </nav>
+    </wrapper>
+  </header>
+</template>
+
+<script>
+import Wrapper from '@/components/Wrapper'
+export default {
+  components: {
+    wrapper: Wrapper
+  }
+}
+</script>
