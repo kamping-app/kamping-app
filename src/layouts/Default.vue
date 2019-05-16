1d51ba6db69808fa654e25b28f2dc173cb77ec1f
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-05-16 00:14:09 -0300

    Create layouts

---
+++ src/layouts/Default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <page-header />
+    <slot />
+    <page-footer />
+  </div>
+</template>
+<script>
+import PageHeader from '@/components/PageHeader'
+import PageFooter from '@/components/PageFooter'
+export default {
+  components: {
+    'page-header': PageHeader,
+    'page-footer': PageFooter
+  }
+}
+</script>
