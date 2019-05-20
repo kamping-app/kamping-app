fc8e584aafa39777f88ef341fd89e4a5c8a79421
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-05-20 12:40:39 -0300

    Add breadcrumbs

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="mb-3">
+    <li
+      class="inline-block text-xs  text-grey-400"
+      v-for="(link, index) in links"
+      :key="index"
+    >
+      <router-link class="text-yellow-vivid-600" :to="`${link.url}`">{{
+        link.label
+      }}</router-link
+      ><span class="mx-1">&bull;</span>
+    </li>
+    <li class="inline-block text-grey-600 text-xs">{{ current }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['current', 'links'],
+  methods: {
+    firstItem(item) {
+      if (item === 0) return 'pl-0'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/City.vue
+++ src/views/City.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="page-city">
     <wrapper>
+      <breadcrumbs
+        :links="[
+          { label: city.country.name, url: `/${city.country.slug}` },
+          {
+            label: city.state.name,
+            url: `/${city.country.slug}/${city.state.slug}/${
+              city.state.hashids
+            }`
+          }
+        ]"
+        :current="city.name"
+      />
       <h1 v-if="!loading" class="text-xl md:text-3xl font-bold text-grey-500">
         {{ city.name }}
       </h1>
@@ -23,12 +35,14 @@
 <script>
 import axios from 'axios'
 import CampingCard from '@/components/CampingCard'
+import Breadcrumbs from '@/components/Breadcrumbs'
 import Wrapper from '@/components/Wrapper'
 
 export default {
   props: ['city_slug', 'city_id'],
   components: {
     'camping-card': CampingCard,
+    breadcrumbs: Breadcrumbs,
     wrapper: Wrapper
   },
   data() {
@@ -36,7 +50,16 @@ export default {
       loading: false,
       city: {
         name: null || '',
-        campings: null
+        campings: null,
+        country: {
+          name: '',
+          slug: ''
+        },
+        state: {
+          name: '',
+          slug: '',
+          hashids: ''
+        }
       }
     }
   },
