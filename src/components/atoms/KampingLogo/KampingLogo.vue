805f5fa63b88c2bded05e89e6426077b142b6dea
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-06-30 21:15:00 -0300

    Create and implements Kamping Logo component

---
+++ src/components/atoms/KampingLogo/KampingLogo.vue
@@ -0,0 +1,22 @@
+<template>
+  <img :src="logo" alt="Kamping logo" class="w-64 mx-auto mb-6" />
+</template>
+
+<script>
+export default {
+  name: 'kamping-logo',
+  props: {
+    logoSize: {
+      type: String,
+      default: 'medium'
+    }
+  },
+  data() {
+    return {
+      logo: require('@/assets/images/kamping-logo.png')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
