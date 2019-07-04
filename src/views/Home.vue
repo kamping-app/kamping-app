74460ef9a98b8c8ec0d2f0ff15e4e648f86cd868
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-07-04 13:57:40 -0300

    Adding KampingLogo component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home h-screen flex items-center">
     <div class="w-full max-w-md mx-auto px-2">
+      <kamping-logo class=" mx-auto" />
       <p class="font-light text-grey-500 text-lg mb-2">
         {{ $t('search.findthebestplace') }}
       </p>
@@ -32,12 +33,15 @@
 </template>
 
 <script>
+import KampingLogo from '@/components/atoms/KampingLogo'
+
 import FeaturedCities from '../components/FeaturedCities'
 import SearchBox from '@/components/molecules/SearchBox'
 
 export default {
   name: 'home',
   components: {
+    'kamping-logo': KampingLogo,
     'featured-cities': FeaturedCities,
     'search-box': SearchBox
   }
