5758ee44e739f56ccb8ddd0f0f45db88e60e56bc
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-07-04 13:52:09 -0300

    SearchBox Component

---
+++ src/components/molecules/SearchBox/SearchBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <generic-input :input-placeholder="$t('search.placeholder')" />
+    <generic-button>
+      {{ $t('search.button') }}
+    </generic-button>
+  </div>
+</template>
+
+<script>
+import GenericButton from '@/components/atoms/GenericButton'
+import GenericInput from '@/components/atoms/GenericInput'
+
+export default {
+  name: 'search-box',
+  components: {
+    'generic-button': GenericButton,
+    'generic-input': GenericInput
+  }
+}
+</script>
