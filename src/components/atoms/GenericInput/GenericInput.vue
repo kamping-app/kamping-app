50491eda554212aac07567c12f8af1e825a83f5c
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-07-04 13:51:53 -0300

    Generic Input component

---
+++ src/components/atoms/GenericInput/GenericInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <input
+    :type="inputType"
+    :name="inputName"
+    :id="inputId"
+    class="border border-grey-100  px-2 py-2 text-grey-500 text-lg font-light w-full"
+    :placeholder="inputPlaceholder"
+  />
+</template>
+
+<script>
+export default {
+  name: 'generic-input',
+  props: {
+    inputType: {
+      type: String,
+      default: 'text'
+    },
+
+    inputName: {
+      type: String,
+      default: 'input'
+    },
+
+    inputId: {
+      type: String,
+      default: 'input'
+    },
+
+    inputPlaceholder: {
+      type: String,
+      default: 'text'
+    },
+  }
+}
+</script>
