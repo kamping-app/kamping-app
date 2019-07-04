d89a0c1e31b6ab1209666ffb0660af76b84d24bc
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-07-04 13:57:26 -0300

    Remove hardcode logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="home h-screen flex items-center">
     <div class="w-full max-w-md mx-auto px-2">
-      <img
-        alt="Kamping logo"
-        class="w-64 mx-auto mb-6"
-        src="../assets/images/kamping-logo.png"
-      />
       <p class="font-light text-grey-500 text-lg mb-2">
         {{ $t('search.findthebestplace') }}
       </p>
