bc93530b506820342e943f3552bf26118448204a
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-07-13 12:42:27 -0300

    Refactoring using arrow functions

--- src/services/city.services.js
+++ src/services/city.services.js
@@ -2,24 +2,16 @@ import Axios from 'axios'
 
 const RESOURCE_NAME = `/cities`
 
+const getAll = () => Axios.get(RESOURCE_NAME)
+const get = id => Axios.get(`${RESOURCE_NAME}/${id}`)
+const create = data => Axios.post(RESOURCE_NAME, data)
+const update = (id, data) => Axios.put(`${RESOURCE_NAME}/${id}`, data)
+const destroy = id => Axios.delete(`${RESOURCE_NAME}/${id}`)
+
 export default {
-  getAll() {
-    return Axios.get(RESOURCE_NAME)
-  },
-
-  get(id) {
-    return Axios.get(`${RESOURCE_NAME}/${id}`)
-  },
-
-  create(data) {
-    return Axios.post(RESOURCE_NAME, data)
-  },
-
-  update(id, data) {
-    return Axios.put(`${RESOURCE_NAME}/${id}`, data)
-  },
-
-  delete(id) {
-    return Axios.delete(`${RESOURCE_NAME}/${id}`)
-  }
+  getAll,
+  get,
+  create,
+  update,
+  destroy
 }
