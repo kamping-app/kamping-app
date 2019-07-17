5c78c8931d533abe3cbd2d6cbefe9cc464beb5f6
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-07-17 00:08:01 -0300

    Create Campings Service

---
+++ src/services/camping.services.js
@@ -0,0 +1,17 @@
+import Axios from 'axios'
+
+const RESOURCE_NAME = `/campings`
+
+const getAll = () => Axios.get(RESOURCE_NAME)
+const get = id => Axios.get(`${RESOURCE_NAME}/${id}`)
+const create = data => Axios.post(RESOURCE_NAME, data)
+const update = (id, data) => Axios.put(`${RESOURCE_NAME}/${id}`, data)
+const destroy = id => Axios.delete(`${RESOURCE_NAME}/${id}`)
+
+export default {
+  getAll,
+  get,
+  create,
+  update,
+  destroy
+}
